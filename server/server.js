272968928491875dbc0502ba557084d753c5c025
Yujia Lian <yujia.lian001@gmail.com>
2018-01-22 12:32:48 -0500

    Login funciton

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const userRouter = require('./user')
 const app = express()
+app.use(cookieParser())
+app.use(bodyParser.json())
 app.use('/user',userRouter)//Enable middleware, anything relate to user prefix, child router is defined in
 //user router.
 app.listen(9093,function() {
--- server/user.js
+++ server/user.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const Router = express.Router()
 const models = require('./model')
-const User = model.getModel('user')
+const User = models.getModel('user')
 
 Router.get('/list',function(req, res) {
   User.find({}, function(err, doc) {
     return res.json(doc)
+  })
+})
+Router.post('/register', function(req, res) {
+  console.log("req body is:", req.body)
+  const {user, pwd, type} = req.body
+  User.findOne({user}, function(err,doc) {//In es6 User.findOne({user:user} can be writen as: findOne({user}
+    if(doc) {
+      return res.json({code:1, msg:'Duplicate username!'})
+    }
+    User.create({user,pwd,type}, function(err,doc) {
+      if(err) {
+        return res.json({code:1, msg:'Back-end err!'})
+      }
+
+      return res.json({code:0})
+    })
   })
 })
 Router.get('/info',function(req, res) {
